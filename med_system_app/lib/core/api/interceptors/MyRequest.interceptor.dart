2446a3ed8ee2ecc4a6b3ba773fd8c3ecb84aea58
Roanderson Pinheiro <roandersonfelipepinheiro@gmail.com>
2024-01-06 13:26:13 -0300

    chore: added interceptors

--- med_system_app/lib/core/api/interceptors/MyRequest.interceptor.dart
+++ med_system_app/lib/core/api/interceptors/MyRequest.interceptor.dart
@@ -1,9 +1,22 @@
 import 'dart:async';
 import 'package:chopper/chopper.dart';
+import 'package:get_it/get_it.dart';
+import 'package:med_system_app/core/modules/signin/store/signin.store.dart';
 
 class MyRequestInterceptor implements RequestInterceptor {
+  final signStore = GetIt.I.get<SignInStore>();
+
   @override
   FutureOr<Request> onRequest(Request request) async {
+    Uri uri = request.url;
+
+    if (!uri.toString().contains('sign_in')) {
+      if (signStore.currentUser?.token != null) {
+        return applyHeaders(request,
+            {'Authorization': 'Bearer ${signStore.currentUser!.token!}'});
+      }
+    }
+
     return request;
   }
 }
