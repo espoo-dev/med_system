aca9fbc5bf6208849095e8bec98823dd0491525c
Roanderson Pinheiro <roandersonfelipepinheiro@gmail.com>
2024-01-30 21:57:42 -0300

    :chore: added filters to the procedures event listing screen

--- med_system_app/lib/features/event_procedures/pages/event_procedures_page.dart
+++ med_system_app/lib/features/event_procedures/pages/event_procedures_page.dart
@@ -26,6 +26,7 @@ class _EventProceduresPageState extends State<EventProceduresPage> {
   List<EventProcedures>? _listEventProcedures = [];
   final ScrollController _scrollController = ScrollController();
   bool isFab = false;
+
   @override
   void initState() {
     super.initState();
@@ -87,78 +88,150 @@ class _EventProceduresPageState extends State<EventProceduresPage> {
                 to(context, const AddEventProcedurePage());
               },
             ),
-      body: RefreshIndicator(
-        onRefresh: _refreshProcedures,
-        child: Observer(
-          builder: (BuildContext context) {
-            if (eventProcedureStore.state == EventProcedureState.error) {
-              return Center(
-                  child: ErrorRetryWidget(
-                      'Algo deu errado', 'Por favor, tente novamente', () {
-                eventProcedureStore.getAllEventProcedures(isRefresh: true);
-              }));
-            }
-            if (eventProcedureStore.state == EventProcedureState.loading &&
-                _listEventProcedures!.isEmpty) {
-              return const Center(child: CircularProgressIndicator());
-            }
-            if (eventProcedureStore.eventProcedureList.isEmpty) {
-              return const Center(
-                  child: Text(
-                      'Você não possui eventos procedimentos cadastrados.'));
-            }
-            _listEventProcedures = eventProcedureStore.eventProcedureList;
-            return Stack(
-              children: [
-                ListView.separated(
-                    controller: _scrollController,
-                    itemCount:
-                        eventProcedureStore.state == EventProcedureState.loading
-                            ? _listEventProcedures!.length + 1
-                            : _listEventProcedures!.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      if (index < _listEventProcedures!.length) {
-                        EventProcedures eventProcedures =
-                            _listEventProcedures![index];
-                        return ListTile(
-                          onTap: () {
-                            to(
-                                context,
-                                EditEventProcedurePage(
-                                    eventProcedures: eventProcedures));
-                          },
-                          leading: SvgPicture.asset(
-                            eventProcedures.isPaid()
-                                ? iconCheckCoreAsset
-                                : iconCloseCoreAsset,
-                            width: 32,
-                            height: 32,
-                            color: eventProcedures.isPaid()
-                                ? Theme.of(context).colorScheme.primary
-                                : const Color(0xFFEC2A58),
-                          ),
-                          title: Text(
-                            eventProcedures.procedure ?? "",
-                            style: const TextStyle(
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          subtitle: Text(eventProcedures.patient ?? ""),
-                          trailing: Icon(
-                            size: 10.0,
-                            Icons.arrow_forward_ios,
-                            color: Theme.of(context).colorScheme.primary,
-                          ),
-                        );
-                      } else {
-                        return const Center(child: CircularProgressIndicator());
-                      }
-                    },
-                    separatorBuilder: (_, __) => const Divider()),
-              ],
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Observer(builder: (_) {
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: FilterChip(
+                        label: const Text('Todos'),
+                        selected: eventProcedureStore.showAll!,
+                        onSelected: (selected) {
+                          _refreshProcedures();
+                          eventProcedureStore.updateFilter(
+                              selected, false, false);
+                        },
+                        showCheckmark: false,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: FilterChip(
+                        label: const Text('Pagos'),
+                        selected: eventProcedureStore.showPaid!,
+                        onSelected: (selected) {
+                          _refreshProcedures();
+                          eventProcedureStore.updateFilter(
+                              false, selected, false);
+                        },
+                        showCheckmark: false,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: FilterChip(
+                        label: const Text('Não pagos'),
+                        selected: eventProcedureStore.showUnpaid!,
+                        onSelected: (selected) {
+                          _refreshProcedures();
+                          eventProcedureStore.updateFilter(
+                              false, false, selected);
+                        },
+                        showCheckmark: false,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: FilterChip(
+                        label: const Text('Mês'),
+                        selected: false,
+                        onSelected: (selected) {},
+                        showCheckmark: false,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             );
-          },
-        ),
+          }),
+          Expanded(
+            child: RefreshIndicator(
+              onRefresh: _refreshProcedures,
+              child: Observer(
+                builder: (BuildContext context) {
+                  if (eventProcedureStore.state == EventProcedureState.error) {
+                    return Center(
+                        child: ErrorRetryWidget(
+                            'Algo deu errado', 'Por favor, tente novamente',
+                            () {
+                      eventProcedureStore.getAllEventProcedures(
+                          isRefresh: true);
+                    }));
+                  }
+                  if (eventProcedureStore.state ==
+                          EventProcedureState.loading &&
+                      _listEventProcedures!.isEmpty) {
+                    return const Center(child: CircularProgressIndicator());
+                  }
+                  if (eventProcedureStore.eventProcedureList.isEmpty) {
+                    return const Center(
+                        child: Text(
+                            'Você não possui eventos procedimentos cadastrados.'));
+                  }
+                  _listEventProcedures = eventProcedureStore.eventProcedureList;
+                  return Stack(
+                    children: [
+                      ListView.separated(
+                          controller: _scrollController,
+                          itemCount: eventProcedureStore.state ==
+                                  EventProcedureState.loading
+                              ? _listEventProcedures!.length + 1
+                              : _listEventProcedures!.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            if (index < _listEventProcedures!.length) {
+                              EventProcedures eventProcedures =
+                                  _listEventProcedures![index];
+                              return ListTile(
+                                onTap: () {
+                                  to(
+                                      context,
+                                      EditEventProcedurePage(
+                                          eventProcedures: eventProcedures));
+                                },
+                                leading: SvgPicture.asset(
+                                  eventProcedures.isPaid()
+                                      ? iconCheckCoreAsset
+                                      : iconCloseCoreAsset,
+                                  width: 32,
+                                  height: 32,
+                                  color: eventProcedures.isPaid()
+                                      ? Theme.of(context).colorScheme.primary
+                                      : const Color(0xFFEC2A58),
+                                ),
+                                title: Text(
+                                  eventProcedures.procedure ?? "",
+                                  style: const TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                subtitle: Text(eventProcedures.patient ?? ""),
+                                trailing: Icon(
+                                  size: 10.0,
+                                  Icons.arrow_forward_ios,
+                                  color: Theme.of(context).colorScheme.primary,
+                                ),
+                              );
+                            } else {
+                              return const Center(
+                                  child: CircularProgressIndicator());
+                            }
+                          },
+                          separatorBuilder: (_, __) => const Divider()),
+                    ],
+                  );
+                },
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
