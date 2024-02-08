c3adf5d7b9dacb9aae2cb90718a4eb4a70af403c
Roanderson Pinheiro <roandersonfelipepinheiro@gmail.com>
2024-02-08 15:26:13 -0300

    chore: injecting add hospital store

--- med_system_app/lib/features/hospitals/respository/hospital_repository.dart
+++ med_system_app/lib/features/hospitals/respository/hospital_repository.dart
@@ -5,6 +5,8 @@ import 'package:med_system_app/core/api/api_result.dart';
 import 'package:med_system_app/core/api/network_exceptions.dart';
 import 'package:med_system_app/features/hospitals/model/hospital.model.dart';
 
+import '../model/add_hospital_request.model.dart';
+
 class HospitalRepository {
   Future<Result<List<Hospital>?>?> getAllHospitals(
       [int? page, int? perPage]) async {
@@ -18,6 +20,28 @@ class HospitalRepository {
             HospitalModel.fromJson(json.decode(response.body));
 
         return Result.success(hospitalModel.hospitalList);
+      } else if (response.statusCode == 500) {
+        return Result.failure(NetworkExceptions.getException(
+            const NetworkExceptions.internalServerError()));
+      }
+    } catch (e) {
+      throw Result.failure(NetworkExceptions.getException(e));
+    }
+    return null;
+  }
+  Future<Result<Hospital>?> registerHospital(
+      AddHospitalRequestModel addHospitalRequestModel) async {
+    try {
+      final response = await hospitalService
+          .registerHospital(json.encode(addHospitalRequestModel.toJson()));
+
+      if (response.isSuccessful) {
+        Hospital? hospital = Hospital.fromJson(json.decode(response.body));
+
+        return Result.success(hospital);
+      } else if (response.statusCode == 422) {
+        return Result.failure(NetworkExceptions.getException(
+            const NetworkExceptions.unableToProcess()));
       } else if (response.statusCode == 500) {
         return Result.failure(NetworkExceptions.getException(
             const NetworkExceptions.internalServerError()));
