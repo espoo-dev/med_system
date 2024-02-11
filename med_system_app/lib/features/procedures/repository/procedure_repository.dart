28d0788b1afb38b90832f52f5922f4de519f8847
Roanderson Pinheiro <roandersonfelipepinheiro@gmail.com>
2024-02-11 20:30:38 -0300

    chore: added method to register procedure in the repository

--- med_system_app/lib/features/procedures/repository/procedure_repository.dart
+++ med_system_app/lib/features/procedures/repository/procedure_repository.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:med_system_app/core/api/api.dart';
 import 'package:med_system_app/core/api/api_result.dart';
 import 'package:med_system_app/core/api/network_exceptions.dart';
+import 'package:med_system_app/features/procedures/model/add_procedure.model.dart';
 import 'package:med_system_app/features/procedures/model/procedure.model.dart';
 
 class ProcedureRepository {
@@ -14,6 +15,29 @@ class ProcedureRepository {
             ProcedureModel.fromJson(json.decode(response.body));
 
         return Result.success(procedureModel.procedureList);
+      } else if (response.statusCode == 500) {
+        return Result.failure(NetworkExceptions.getException(
+            const NetworkExceptions.internalServerError()));
+      }
+    } catch (e) {
+      throw Result.failure(NetworkExceptions.getException(e));
+    }
+    return null;
+  }
+
+  Future<Result<Procedure>?> registerProcedure(
+      AddProcedureRequestModel addProcedureRequestModel) async {
+    try {
+      final response = await procedureService
+          .registerProcedure(json.encode(addProcedureRequestModel.toJson()));
+
+      if (response.isSuccessful) {
+        Procedure? procedure = Procedure.fromJson(json.decode(response.body));
+
+        return Result.success(procedure);
+      } else if (response.statusCode == 422) {
+        return Result.failure(NetworkExceptions.getException(
+            const NetworkExceptions.unableToProcess()));
       } else if (response.statusCode == 500) {
         return Result.failure(NetworkExceptions.getException(
             const NetworkExceptions.internalServerError()));
