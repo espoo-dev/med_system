b70ba358ace73a7e5d6ac6edf3c1b9ff04808fb2
Roanderson Pinheiro <roandersonfelipepinheiro@gmail.com>
2024-02-17 22:02:47 -0300

    chore: layout adjustments

--- med_system_app/lib/features/home/pages/home_page.dart
+++ med_system_app/lib/features/home/pages/home_page.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:get_it/get_it.dart';
+import 'package:med_system_app/core/theme/icons.dart';
+import 'package:med_system_app/core/utils/navigation_utils.dart';
 import 'package:med_system_app/core/utils/ui.dart';
 import 'package:med_system_app/core/widgets/error.widget.dart';
 import 'package:med_system_app/core/widgets/my_horizontal_menu.widget.dart';
 import 'package:med_system_app/features/event_procedures/model/event_procedure.model.dart';
+import 'package:med_system_app/features/event_procedures/pages/add_event_procedure_page.dart';
 import 'package:med_system_app/features/home/model/menu_home.model.dart';
 import 'package:med_system_app/features/home/store/home.store.dart';
 import 'package:med_system_app/features/home/widgets/build_header.widget.dart';
 import 'package:med_system_app/features/home/widgets/build_welcome.widget.dart';
 import 'package:med_system_app/features/home/widgets/list_events.widget.dart';
 import 'package:med_system_app/features/home/widgets/my_drawer.widget.dart';
+import 'package:med_system_app/features/patients/pages/patient_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -99,6 +104,43 @@ class _HomePageState extends State<HomePage> {
       child: Material(
         child: Scaffold(
           key: _scaffoldKey,
+          floatingActionButtonLocation:
+              FloatingActionButtonLocation.centerDocked,
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              push(context, const AddEventProcedurePage());
+            },
+            backgroundColor: Theme.of(context).colorScheme.primary,
+            child: const Icon(
+              Icons.add,
+              color: Colors.white,
+            ),
+          ),
+          bottomNavigationBar: BottomAppBar(
+              notchMargin: 5.0,
+              shape: const CircularNotchedRectangle(),
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    IconButton(
+                      icon: SvgPicture.asset(
+                        iconMenuPlantao,
+                        width: 24,
+                        height: 24,
+                      ),
+                      onPressed: () {},
+                    ),
+                    IconButton(
+                      icon: SvgPicture.asset(
+                        iconMenuPatient,
+                        width: 24,
+                        height: 24,
+                      ),
+                      onPressed: () {
+                        to(context, const PatientPage());
+                      },
+                    ),
+                  ])),
           drawer: const MyDrawer(),
           body: SafeArea(
             child: Stack(
